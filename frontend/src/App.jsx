8a6dc90c03f1845e77ca290079ac7c58e791345a
roshaans2 <roshaansathya@gmail.com>
2024-02-14 22:09:08 +0530

    Done with Routing in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Dashboard from './components/Dashboard'
+import Signup from './components/Signup'
+import Signin from './components/Signin'
+import SendMoney from './components/SendMoney'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/signin' element={<Signin/>}></Route>
+          <Route path='/send' element={<SendMoney/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
